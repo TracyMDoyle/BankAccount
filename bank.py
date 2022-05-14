873c1bead9c2a4c1bec667ece72309edf84bb442
Tracy Doyle <doyletracy@hotmail.com>
2022-05-14 09:21:30 -0400

    update to add user class to Bank Account

--- bank.py
+++ bank.py
@@ -30,7 +30,39 @@ class BankAccount:
     def all_accounts(cls):
         for Accounts in cls.Accounts:
             Accounts.display_account_info()
+        
+class User:
+    def __init__(self, name):
+        self.name = name
+        self.account = BankAccount(int_rate=0.02, balance=0)
+    
+    def make_deposit(self, amount):
+        self.account.balance += amount
+        # print(self.account.balance) check and it works
+        return self
 
+    def make_withdraw(self, amount):
+        self.account.balance -= amount
+        # print(self.account.balance) check and it works
+        return self
+
+    def display_user_balance(self):
+        print(f"Your Current Balance: {self.account.balance}")
+        return self
+
+Al = User("Al")
+
+Al.make_deposit(200)
+Al.make_withdraw(50)
+
+
+Al.display_user_balance()
+
+
+
+
+'''
+previous assignment code before updating to add a user class
 
 account1 = BankAccount(0.05, 120)
 account2 = BankAccount(0.05, 400)
@@ -39,7 +71,7 @@ account1.deposit(10).deposit(10).deposit(10).withdraw(5).yield_interest(0.05).di
 account2.deposit(100).deposit(500).withdraw(250).withdraw(20).withdraw(10).withdraw(20).yield_interest(0.05).display_account_info()
 
 BankAccount.all_accounts()
-
+'''
 
 
 
